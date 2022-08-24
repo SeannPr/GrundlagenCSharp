41660d1f9912ee74670ed94ca62e2903cf3dccd4
seanpaul.p <seanpaul.p@P-0G-0B005-0390>
2022-08-24 12:23:11 +0200

    Projektdateien hinzufügen.

---
+++ Konsolenausgabe/Program.cs
@@ -0,0 +1,40 @@
+﻿class myProgram
+{
+    public static void Main()
+    {
+        Aufgabe2();
+        Aufgabe1();
+        Aufgabe3();
+    }
+
+
+    static void Aufgabe1()
+    {
+
+        Console.WriteLine(" .-.");
+        Console.WriteLine("(O O)");
+        Console.WriteLine("| 0 |");
+        Console.WriteLine("|   |");
+        Console.WriteLine("´~~~´");
+
+    }
+
+    static void Aufgabe2()
+    {
+
+        string Vorname = "Sean";
+        string Nachname = "Prior";
+        string Alter = "16";
+
+
+        Console.WriteLine(Vorname);
+        Console.WriteLine(Nachname);
+        Console.WriteLine(Alter);
+
+
+    }
+
+    static void Aufgabe3()
+
+
+}
